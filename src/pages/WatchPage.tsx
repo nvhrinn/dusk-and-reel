cad15d28790b64adb6e4c3eff7bba7d96c5a8c47
Rinn. <167101710+nvhrinn@users.noreply.github.com>
2026-03-06 15:22:01 +0700

    Update WatchPage.tsx

--- src/pages/WatchPage.tsx
+++ src/pages/WatchPage.tsx
@@ -79,7 +79,7 @@ const WatchPage = () => {
   const epId = params.get("ep");
   const navigate = useNavigate();
   const [user, setUser] = useState<any>(getUser());
-const [coupons, setCoupons] = useState(0);
+const [coupons, setCouponsState] = useState(0);
   const [selectedSourceId, setSelectedSourceId] = useState<string | null>(null);
   const [audioType, setAudioType] = useState<"sub" | "dub">("sub");
   const [cachedSubTracks, setCachedSubTracks] = useState<{ file: string; label: string; kind: string }[]>([]);
