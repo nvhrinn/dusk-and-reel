9f444c5278d99e6074cdacc4faaf6cdaa448f4c9
Rinn. <167101710+nvhrinn@users.noreply.github.com>
2026-04-04 18:54:47 +0700

    Update client2.ts

--- src/integrations/supabase/client2.ts
+++ src/integrations/supabase/client2.ts
@@ -8,7 +8,7 @@ const SUPABASE_PUBLISHABLE_KEY = import.meta.env.SUPABASE_TRANSLATE_SERVICE_ROLE
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase2 = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
